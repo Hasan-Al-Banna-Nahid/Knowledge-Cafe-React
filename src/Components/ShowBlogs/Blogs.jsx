15c29e3988fd5b469370c9aa0949995c1995e67d
Hasan-AL-Banna-Nahid <Iamnahid591998@gmail.com>
2023-03-31 01:39:49 +0600

    Showing Blogs Section On UI Done Without Responsiveness

--- src/Components/ShowBlogs/Blogs.jsx
+++ src/Components/ShowBlogs/Blogs.jsx
@@ -6,20 +6,22 @@ const Blogs = (props) => {
     const { image, AuthorName, AuthorImage, Date, title, ReadTime, hashtags, } = props.blogs;
     return (
         <div>
-            <div>
+            <div className='card'>
                 <img id='title-image' src={image} alt="" />
                 <div>
                     <div className='author-info'>
                         <div className='author-data' >
                             <img id='author-image' src={AuthorImage} alt="" />
-                            <h5 id='author-name'>{AuthorName}</h5>
-                            <p id='date'>{Date}</p>
+                            <div className='authorName-Date'>
+                                <p id='author-name'>{AuthorName}</p>
+                                <p id='date'>{Date}</p>
+                            </div>
                         </div>
                         <p id='read-time'>{ReadTime} <FontAwesomeIcon icon={faBookmark} /></p>
                     </div>
-                        <h5 id='title'>{title}</h5>
-                        <p id='hashtag'>{hashtags}</p>
-                        <a href="">Mark As Read</a>
+                    <h3 id='title'>{title}</h3>
+                    <p id='hashtag'>{hashtags}</p>
+                    <a href="">Mark As Read</a>
                 </div>
             </div>
         </div>
